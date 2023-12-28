a8c1ca8467484efd353ad6d882334934d4e16c2a
周群辉 <zhouqunhui@zhouqunhuideMacBook-Pro.local>
2023-12-28 21:08:18 +0800

    first commit code

---
+++ js/lov.js
@@ -0,0 +1,9 @@
+jQuery(window).load(function() {
+})
+
+jQuery(document).ready(function(){
+	jQuery("#_nav_top").load("_top.html");
+	jQuery("#_navbar").load("_navbar.html");
+	jQuery("#_footer").load("_footer.html");
+
+})
